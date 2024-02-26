7a59ef9f238f360f3572357480a57447f9d155c3
alyaanafea <alyaanafea@gmail.com>
2024-02-26 15:17:15 +0200

    updating:login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputComponent } from './components/input/input.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InputComponent],
+  imports: [RouterOutlet, InputComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
