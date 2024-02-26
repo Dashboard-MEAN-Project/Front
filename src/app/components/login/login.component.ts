0dae84dbac50310dc311df0795a80b34e2fee7fa
alyaanafea <alyaanafea@gmail.com>
2024-02-26 17:32:36 +0200

    updating auth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,30 +7,39 @@ import {
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, ReactiveFormsModule, CommonModule],
+  imports: [InputComponent, ReactiveFormsModule, CommonModule,HttpClientModule],
+  providers:[AuthService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private authService:AuthService){
+
+  }
   loginForm: FormGroup = new FormGroup({
     email: new FormControl('', [
       Validators.required,
-      Validators.minLength(3),
+      Validators.email,
+      
     ]),
     password: new FormControl('', [
       Validators.required,
-      Validators.minLength(8),
+      Validators.minLength(3),
+      Validators.maxLength(30)
     ]),
   });
 
   onSubmit(event: Event) {
     event.preventDefault();
     if (this.loginForm.status === 'VALID') {
-      console.log(this.loginForm.value);
+      const user = {...this.loginForm.value}
+      this.authService.login(user)
     } else {
       this.loginForm.markAllAsTouched();
       console.log('not valids');
