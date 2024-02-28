5656fc090228f0277e0ed6f42aff8ccabead211c
alyaanafea <alyaanafea@gmail.com>
2024-02-28 14:37:04 +0200

    creating users table

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../../model/User';
 const users_db = 'http://localhost:5000/users';
+const editUser_db = 'http://localhost:5000/users/users';
+const deleteUsers_db = 'http://localhost:5000/Users/users';
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +13,17 @@ export class UserService {
     return this.myclient.get<User[]>(users_db, {
       headers: requestHeaders,
     });
+  }
+
+  getUserById(id: number) {
+    return this.myclient.get(users_db + '/' + id);
+  }
+  updateUserDetails(id: number, newUserDetails: any) {
+    return this.myclient.put(editUser_db + '/' + id, { ...newUserDetails });
+  }
+  DeleteUser(id: number) {
+    // console.log('deleeet');
+    return this.myclient.delete(deleteUsers_db + '/' + id);
   }
 }
 
