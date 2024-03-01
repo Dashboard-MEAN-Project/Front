e66a1e1beec35b00c309e3c801f2506401a1a2e3
alyaanafea <alyaanafea@gmail.com>
2024-03-01 15:26:41 +0200

    create home user

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-const products_db = 'http://localhost:5000/cont/content';
+const products_db = 'http://localhost:5000/cont/getAllProduct';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  constructor(private myproduct: HttpClient) { }
+  constructor(private myproduct: HttpClient) {}
   getAllProductss() {
-    return this.myproduct.get(products_db);
-    
+    return this.myproduct.get(products_db, {
+      headers: requestHeaders,
+    });
   }
-
-
 }
+const token = localStorage.getItem('token');
+const requestHeaders = {
+  Authorization: `Bearer ${token}`,
+};
